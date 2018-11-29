a83530c5fbf91e621d1cba7ff9b0ceb8a2737890
Howarddful123 <Howarddful123@gmail.com>
2018-11-29 14:52:29 -0800

    -Added shooting mechanic. -Added placeholder for gun sprite.

---
+++ Cooking-Lab/Assets/Scripts/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    private float lifetime = 2f;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = transform.right * speed;
+	}
+}
